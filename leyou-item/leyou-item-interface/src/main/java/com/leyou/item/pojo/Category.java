e5002da77e93322fa35e98547e08d02e03582c4e
8199586171 <819958617@qq.com>
2021-01-26 08:32:49 +0800

    123

---
+++ leyou-item/leyou-item-interface/src/main/java/com/leyou/item/pojo/Category.java
@@ -0,0 +1,18 @@
+package com.leyou.item.pojo;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name="tb_category")
+public class Category {
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Long parentId;
+	private Boolean isParent; // 注意isParent生成的getter和setter方法需要手动加上Is
+	private Integer sort;
+	// getter和setter略
+}
